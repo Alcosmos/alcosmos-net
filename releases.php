be06ac50f2937d46ca7949ddca7c24b8eab0840e
Alcosmos <67235982+Alcosmos@users.noreply.github.com>
2021-04-17 20:36:17 +0200

    Now it loads the repos from the SQL database

--- releases.php
+++ releases.php
@@ -1,65 +1,17 @@
 <?php
 	/*\
-	 * Lists the repositories from the given GitHub account along with some data about them
+	 * Lists the repositories from the database
 	 * Structured and formatted for alcosmos.ddns.net?tab=releases
 	 * 
 	 * A part of alcosmos.ddns.net
 	\*/
 	
-				$reposURL = file_get_contents('https://api.github.com/users/Alcosmos/repos'); # ('data/repos');
+				$query = $pdo -> prepare("SELECT * FROM config WHERE id = ?;");
+				$query -> execute(array("releases"));
 				
-				if ($reposURL === FALSE) {
-					echo '<p>
-						Bad news; GitHub limits their API to only 5000 requests per hour, and my website has already reached that number.
-					</p>
-					<p>
-						Just go straight to <a href="https://github.com/Alcosmos" target="_blank">my GitHub</a>.
-					</p>';
-					
-					return;
-				}
+				$row = $query -> fetch(PDO::FETCH_OBJ);
 				
-				$i = 1;
-				
-				$repos = json_decode($reposURL, true);
-				
-				foreach ($repos as $value) {
-					print('
-			<p class="title">
-				' . $value['name'] . '
-			</p>
-			<p>
-				<b>&nbsp;');
-				
-				if ($value['language'] != '') {
-					print ($value['language'].', ');
-				}
-				
-				if (isset($value['license']['name'])) {
-					$license = json_decode(file_get_contents($value['license']['url']), true);
-					
-					if (isset($license['html_url'])) {
-						print ('<a href="' . $license['html_url'] . '" target="_blank">' . $value['license']['name'] . '</a>');
-					} else {
-						print ($value['license']['name']);
-					}
-				} else {
-					print ('public domain');
-				}
-
-				print('</a></b>
-			</p>
-			<p>
-				' . $value['description'] . '
-			</p>
-			<p>
-				<a href="' . $value['html_url'] . '" target="_blank">GitHub</a> | <a href="' . $value['html_url'] . '/archive/' . $value['default_branch'] . '.zip" target="_blank">Download</a>
-			</p>');
-					
-					if ($i < count($repos)) {
-						print('<hr>');
-					}
-					
-					$i++;
+				if ($row != null) {
+					echo $row->value;
 				}
 ?>
